553f9225e1460ab540c43c924c27099df4228fe7
Dakota Knutson <dakota@ddev.im>
2014-10-01 11:20:54 +0400

    Added code to install tables in database.

--- db.php
+++ db.php
@@ -4,4 +4,44 @@
 
 // Include this page anytime database access is required.
 
+//$path = realpath(dirname(__FILE__));
+$path = "/var/www/ddev.im/projects";
 
+try{
+    // Connect to the SQLite Database
+    $db = new PDO("sqlite:$path/passwordDB.sqlite");
+}
+catch(PDOException $e){
+    echo $e->getMessage();
+}
+
+// Install the Database if required.
+if(isset($_GET['action']){
+    switch $_GET['action']{
+        case "install";
+            dbinstall();
+            break;
+        case default:
+            echo "Action not found!";
+            break;
+    }
+}
+
+
+function dbinstall(){
+    // Install the database  
+
+    // Create the users table
+    $db->exec("CREATE TABLE IF NOT EXISTS users(
+        id integer primary key,
+        username text,
+        publicKey text,
+        privateKey text
+    )");
+
+    $db->exec("CREATE TABLE IF NOT EXISTS passwords(
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        password BLOB
+    )");
+}
