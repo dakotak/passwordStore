e7bfdb2a9fbb30bdd671e2fd1bd3a5486826287f
Dakota Knutson <dakota@ddev.im>
2014-10-01 12:17:15 +0400

    Fixed PHP and SQL syntax errors.

--- db.php
+++ db.php
@@ -9,29 +9,35 @@ $path = "/var/www/ddev.im/projects";
 
 try{
     // Connect to the SQLite Database
-    $db = new PDO("sqlite:$path/passwordDB.sqlite");
+    $db = new PDO("sqlite:$path/passwordDB.sqlite", null, null, array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ));
 }
 catch(PDOException $e){
     echo $e->getMessage();
 }
 
+
 // Install the Database if required.
-if(isset($_GET['action']){
-    switch $_GET['action']{
+if(isset($_GET['action'])){
+    switch ($_GET['action']){
         case "install";
-            dbinstall();
+            echo "Installing Database...<br />";
+            dbinstall($db);
             break;
         case "showTables":
             dbShowTables();
             break;
-        case default:
+        default:
             echo "Action not found!";
             break;
     }
 }
 
 
-function dbinstall(){
+function dbinstall($db){
     // Install the database  
 
     // Create the users table
@@ -41,11 +47,19 @@ function dbinstall(){
         publicKey text,
         privateKey text
     )");
-
+    
+    // Create the passwords table
     $db->exec("CREATE TABLE IF NOT EXISTS passwords(
         id INTEGER PRIMARY KEY,
         title TEXT,
-        password BLOB
+        encryptedPass BLOB
+    )");
+
+    // Create the passwordKeys table to store the encryptedKey used to decrypt the password, key is encrypted using users public key
+    $db->exec("CREATE TABLE IF NOT EXISTS passwordKeys(
+        passwordID INTEGER,
+        userID INTEGER,
+        encryptedKey BLOB
     )");
 }
 
