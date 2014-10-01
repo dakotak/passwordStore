d87e72f120fbc0d2b4db0460c5737f6ab6be9411
Dakota Knutson <dakota@ddev.im>
2014-10-01 10:20:45 +0400

    Environment test page.

---
+++ environmentTest.php
@@ -0,0 +1,19 @@
+<?php
+
+//This page will test to make sure that the current environment support all the requirements of this application.
+echo "PHP Version: " . phpversion() . "<br />";
+
+// Test PHP Version, not sure what version I am requireing yet, Though my guess is PHP v5.3 or up
+
+// Make sure that openSSL extentions are enabled.
+
+// Make sure mcrypt extentions are enabled.
+
+// PDO is required and PDO must support SQLite v3 or up
+if(class_exists('PDO'){
+    echo "<b>PDO is not avalible.</b><br />";
+}else{
+    echo "PDO is avalible.<br />";
+}
+// Check SQLite support
+
